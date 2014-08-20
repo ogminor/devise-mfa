31f30cecb4404d3e9dfd73fbd87fef63c2929273
Sam <ogminor@gmail.com>
2014-08-20 22:35:26 +0200

    Update tokens_controller.rb

--- app/controllers/devise_otp/tokens_controller.rb
+++ app/controllers/devise_otp/tokens_controller.rb
@@ -4,8 +4,6 @@ class DeviseOtp::TokensController < DeviseController
   prepend_before_filter :ensure_credentials_refresh
   prepend_before_filter :authenticate_scope!
 
-  protect_from_forgery :except => [:clear_persistence, :delete_persistence]
-
   #
   # Displays the status of OTP authentication
   #
@@ -40,38 +38,6 @@ class DeviseOtp::TokensController < DeviseController
   end
 
   #
-  # makes the current browser persistent
-  #
-  def get_persistence
-    if otp_set_trusted_device_for(resource)
-      otp_set_flash_message :success, :successfully_set_persistence
-    end
-    redirect_to :action => :show
-  end
-
-  #
-  # clears persistence for the current browser
-  #
-  def clear_persistence
-    if otp_clear_trusted_device_for(resource)
-      otp_set_flash_message :success, :successfully_cleared_persistence
-    end
-
-    redirect_to :action => :show
-  end
-
-  #
-  # rehash the persistence secret, thus, making all the persistence cookies invalid
-  #
-  def delete_persistence
-    if otp_reset_persistence_for(resource)
-      otp_set_flash_message :notice, :successfully_reset_persistence
-    end
-
-    redirect_to :action => :show
-  end
-
-  #
   #
   #
   def recovery
@@ -81,18 +47,18 @@ class DeviseOtp::TokensController < DeviseController
 
   private
 
-  def ensure_credentials_refresh
-    ensure_resource!
-    # This code makes me ;_;
-    if (!resource.otp_enabled? and resource.class.otp_authentication_after_sign_in)
-      # Skip password check on initial setup when using OTP as on separate login screen
-    elsif needs_credentials_refresh?(resource)
-      otp_set_flash_message :notice, :need_to_refresh_credentials
-      redirect_to refresh_otp_credential_path_for(resource)
+    def ensure_credentials_refresh
+      ensure_resource!
+      # This code makes me ;_;
+      if (!resource.otp_enabled? and resource.class.otp_authentication_after_sign_in)
+        # Skip password check on initial setup when using OTP as on separate login screen
+      elsif needs_credentials_refresh?(resource)
+        otp_set_flash_message :notice, :need_to_refresh_credentials
+        redirect_to refresh_otp_credential_path_for(resource)
+      end
     end
-  end
 
-  def scope
-    resource_name.to_sym
-  end
+    def scope
+      resource_name.to_sym
+    end
 end
