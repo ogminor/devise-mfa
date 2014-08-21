02a2bc9e976f7e156097881d95a87cc0e89f2cca
Samantha <ogminor@gmail.com>
2014-08-21 15:17:49 +0200

    Update sessions.rb

--- lib/devise_otp_authenticatable/hooks/sessions.rb
+++ lib/devise_otp_authenticatable/hooks/sessions.rb
@@ -6,15 +6,15 @@ module DeviseOtpAuthenticatable::Hooks
     private
       # resource should be challenged for otp
       def otp_challenge_required_on?(resource)
-        return false unless resource.respond_to?(:otp_enabled) && resource.respond_to?(:otp_auth_secret)
-        resource.otp_enabled && !is_otp_trusted_device_for?(resource)
+        return false unless resource.respond_to?(:otp_enabled) and resource.respond_to?(:otp_auth_secret)
+        resource.otp_enabled and !is_otp_trusted_device_for?(resource)
       end
 
       # the resource -should- have otp turned on, but it isn't
       def otp_mandatory_on?(resource)
         return true if resource.class.otp_mandatory
         return false unless resource.respond_to?(:otp_mandatory)
-        resource.otp_mandatory && !resource.otp_enabled
+        resource.otp_mandatory and !resource.otp_enabled
       end
   end
 end
