91dbf617c4ea02e3ee242491f890d26046321b98
Samantha <ogminor@gmail.com>
2014-08-21 20:09:02 +0200

    Update credentials_controller.rb

--- app/controllers/devise_mfa/credentials_controller.rb
+++ app/controllers/devise_mfa/credentials_controller.rb
@@ -3,17 +3,15 @@ class DeviseMfa::CredentialsController < DeviseController
   prepend_before_filter :authenticate_scope!, only: [:get_refresh, :set_refresh]
 
   def get_refresh
-    ensure_resource
     render :refresh
   end
 
   def set_refresh
     recovery = (params[resource_name][:recovery] == 'true') and recovery_enabled?
-    ensure_resource
-    if resource.otp_enabled?
-      if recovery and resource.validate_otp_token(params[resource_name][:token], recovery)
+    if resource.mfa_enabled?
+      if recovery and resource.validate_mfa_token(params[resource_name][:token], recovery)
         done_valid_refresh
-      elsif resource.validate_otp_token(params[resource_name][:token])
+      elsif resource.validate_mfa_token(params[resource_name][:token])
         done_valid_refresh
       else
         failed_refresh
@@ -25,13 +23,13 @@ class DeviseMfa::CredentialsController < DeviseController
 
   private
     def done_valid_refresh
-      otp_refresh_credentials_for(resource)
-      otp_set_flash_message :success, :valid_refresh if is_navigational_format?
-      respond_with resource, location: otp_fetch_refresh_return_url
+      mfa_refresh_credentials_for(resource)
+      mfa_set_flash_message :success, :valid_refresh if is_navigational_format?
+      respond_with resource, location: mfa_fetch_refresh_return_url
     end
 
     def failed_refresh
-      otp_set_flash_message :alert, :invalid_refresh
+      mfa_set_flash_message :alert, :invalid_refresh
       render :refresh
     end
 end
