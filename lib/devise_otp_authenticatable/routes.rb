1fc571026fd3915bcd1f62f198314e6648f2135a
Samantha <ogminor@gmail.com>
2014-08-21 16:34:13 +0200

    Update routes.rb

--- lib/devise_otp_authenticatable/routes.rb
+++ lib/devise_otp_authenticatable/routes.rb
@@ -5,11 +5,11 @@ module ActionDispatch::Routing
     
       def devise_otp(mapping, controllers)
         namespace :otp, :module => :devise_otp do
-          resource :token, :only => [:show, :update, :destroy], :path => mapping.path_names[:token], :controller => controllers[:otp_tokens]
+          resource :token, only: [:show, :update], path: mapping.path_names[:token], controller: controllers[:otp_tokens]
 
-          resource :credential, :path => mapping.path_names[:credentials], :controller => controllers[:otp_credentials] do
-            get :validate, :action => 'get_refresh'
-            put :validate, :action => 'set_refresh'
+          resource :credential, only: [], path: mapping.path_names[:credentials], controller: controllers[:otp_credentials] do
+            get :validate, action: 'get_refresh'
+            put :validate, action: 'set_refresh'
           end
         end
       end
