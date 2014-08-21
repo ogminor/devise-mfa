e8f5ebf30931a64094809ad0a28abcfcae5a7a70
Samantha <ogminor@gmail.com>
2014-08-21 15:05:38 +0200

    Update helpers.rb
    Didn't need to escape since I'm generating locally.

--- lib/devise_otp_authenticatable/controllers/helpers.rb
+++ lib/devise_otp_authenticatable/controllers/helpers.rb
@@ -63,8 +63,6 @@ module DeviseOtpAuthenticatable
       end
 
       def otp_authenticator_token_image(resource)
-        otp_url = Rack::Utils.escape(resource.otp_provisioning_uri)
-        logger.info "#{otp_url}"
         qr = ['data:image/png;base64,', RQRCode::QRCode.new(otp_url, :size => 8, :level => :h ).to_img.resize(250,250).to_blob].pack('A*m').gsub(/\n/, '')
         image_tag(qr, alt: 'OTP Url QRCode')
       end
