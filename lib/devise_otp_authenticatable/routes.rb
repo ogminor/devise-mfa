b889bc6f73f131b4ff1ee487a4c06b6da0c4266d
Samantha <ogminor@gmail.com>
2014-08-21 16:34:21 +0200

    Update routes.rb

--- lib/devise_otp_authenticatable/routes.rb
+++ lib/devise_otp_authenticatable/routes.rb
@@ -2,7 +2,6 @@ module ActionDispatch::Routing
   class Mapper
 
     protected
-    
       def devise_otp(mapping, controllers)
         namespace :otp, :module => :devise_otp do
           resource :token, only: [:show, :update], path: mapping.path_names[:token], controller: controllers[:otp_tokens]
