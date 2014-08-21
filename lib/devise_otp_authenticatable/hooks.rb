94636bda92113b7ac5a706c64d36e1678f3fd40d
Samantha <ogminor@gmail.com>
2014-08-21 17:35:25 +0200

    Update hooks.rb

--- lib/devise_otp_authenticatable/hooks.rb
+++ lib/devise_otp_authenticatable/hooks.rb
@@ -1,13 +1,1 @@
-module DeviseOtpAuthenticatable
-  module Hooks
 
-    autoload :Sessions, 'devise_otp_authenticatable/hooks/sessions.rb'
-
-    class << self
-      def apply
-        Devise::SessionsController.send(:include, Hooks::Sessions)
-      end
-    end
-
-  end
-end
